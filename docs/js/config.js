63f36f305b28832eda059d46ce2a921c07081a21
1524林奕昇 <yslin.1524@ttri.org.tw>
2026-07-18 15:47:35 +0800

    Config: open voting

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,6 +1,6 @@
 window.TTRI_CONFIG = {
   webAppUrl: 'https://script.google.com/macros/s/AKfycbyN8D1SJC7hS-Tlxaf5xohMV1zkuz_JHRM7FRIMjWGhe1ZuHJWkOPYChrJEoUxZHB7i/exec',
   title: '「神隊友的偉岸背影」攝影人氣投稿',
-  votingOpen: false,          // true = 開放投票；false = 關閉投票
+  votingOpen: true,          // true = 開放投票；false = 關閉投票
   votingClosedMessage: '投票尚未開放，請等候通知。'
 };
